7ba9eebeee0d53c0b72678ae75f8da2ed587396d
Nargiza Akunova <nargizaakun@gmail.com>
2023-01-22 18:01:26 +0100

    feat: update docs

--- src/App.js
+++ src/App.js
@@ -84,6 +84,10 @@ const App = () => {
     }
   };
 
+  const goToMainPage = () => {
+    swiperInst.slideTo(mainPageIndex);
+  };
+
   return (
     <Fragment>
       <Swiper
@@ -114,57 +118,9 @@ const App = () => {
             <div>
               <a href="./sources.html">List of sources</a>
 
-              <div className="left" onClick={scrollLeftHandler}>
-                <p> Future</p>
-                <svg
-                  className="actionHint"
-                  width="18px"
-                  height="17px"
-                  viewBox="0 0 18 17"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g
-                    id="prev"
-                    transform="translate(8.500000, 8.500000) scale(-1, 1) translate(-8.500000, -8.500000)"
-                  >
-                    <polygon
-                      className="arrow"
-                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
-                    ></polygon>
-                    <polygon
-                      className="arrow-fixed"
-                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
-                    ></polygon>
-                    <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
-                  </g>
-                </svg>
-              </div>
-              <div className="right" onClick={scrollRightHandler}>
-                <p>Past</p>
-                <svg
-                  className="actionHint"
-                  width="18px"
-                  height="17px"
-                  viewBox="-1 0 18 17"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  // eslint-disable-next-line react/jsx-no-duplicate-props
-                  // xmlns="http://www.w3.org/1999/xlink"
-                >
-                  <g>
-                    <polygon
-                      className="arrow"
-                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
-                    ></polygon>
-                    <polygon
-                      className="arrow-fixed"
-                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
-                    ></polygon>
-                    <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
-                  </g>
-                </svg>
-              </div>
+              <button className="go-to-main" onClick={goToMainPage}>
+                Go to Current Time
+              </button>
             </div>
           </div>
         </SwiperSlide>
