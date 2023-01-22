369a0164808e0af9eb73074f09cbe780fdf05ee8
Nargiza Akunova <nargizaakun@gmail.com>
2023-01-22 17:44:24 +0100

    feat: update docs

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
   const [swiperInst, setSwiperInst] = useState(null);
   const [initialProgress, setInitialProgress] = useState(null);
 
-  const mainPageIndex = isMobile ? 11 : 30;
+  const mainPageIndex = isMobile ? 12 : 31;
 
   const onSwiper = (swiper) => {
     setSwiperInst(swiper);
@@ -108,6 +108,67 @@ const App = () => {
         onSwiper={onSwiper}
         onScroll={onSliderScroll}
       >
+        {/* BREAK */}
+        <SwiperSlide>
+          <div className="thumbContainer sources">
+            <div>
+              <a href="./sources.html">List of sources</a>
+
+              <div className="left" onClick={scrollLeftHandler}>
+                <p> Future</p>
+                <svg
+                  className="actionHint"
+                  width="18px"
+                  height="17px"
+                  viewBox="0 0 18 17"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g
+                    id="prev"
+                    transform="translate(8.500000, 8.500000) scale(-1, 1) translate(-8.500000, -8.500000)"
+                  >
+                    <polygon
+                      className="arrow"
+                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
+                    ></polygon>
+                    <polygon
+                      className="arrow-fixed"
+                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
+                    ></polygon>
+                    <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+                  </g>
+                </svg>
+              </div>
+              <div className="right" onClick={scrollRightHandler}>
+                <p>Past</p>
+                <svg
+                  className="actionHint"
+                  width="18px"
+                  height="17px"
+                  viewBox="-1 0 18 17"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  // eslint-disable-next-line react/jsx-no-duplicate-props
+                  // xmlns="http://www.w3.org/1999/xlink"
+                >
+                  <g>
+                    <polygon
+                      className="arrow"
+                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
+                    ></polygon>
+                    <polygon
+                      className="arrow-fixed"
+                      points="16.3746667 8.33860465 7.76133333 15.3067621 6.904 14.3175671 14.2906667 8.34246869 6.908 2.42790698 7.76 1.43613596"
+                    ></polygon>
+                    <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        {/* PAST EVENTS */}
         {PAST_EVENTS.map((event) => (
           <SwiperSlide key={`past-${event.id}`}>
             <div className={`thumbContainer past ${event.className || ''}`}>
@@ -266,15 +327,6 @@ const App = () => {
             </div>
           </SwiperSlide>
         ))}
-        {/* BREAK */}
-        <SwiperSlide>
-          <div className="thumbContainer sources">
-            <div>
-              <a href="./sources.html">List of sources</a>
-            </div>
-          </div>
-        </SwiperSlide>
-        {/* PAST EVENTS */}
       </Swiper>
     </Fragment>
   );
